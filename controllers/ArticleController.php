02216a1f72d0e0e147753da613c6de7cbcfa72bd
Safuan Yaylyaev <yaylyaevsafuan@gmail.com>
2020-11-19 23:40:30 +0300

    +create migration (create new table); +add migration.txt (Поясняющая запись про миграции); +add model Article +test ORM

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\Article;
 use yii\web\Controller;
 
 // User является id контроллера
@@ -11,7 +12,7 @@ class ArticleController extends Controller
     // по умолчанию используется шаблон main.php
     // для того чтобы использовать свой
     // можно и в экшне задавать, но принято в контроллере
-    public $layout = 'article';
+    //public $layout = 'article';
 
     public function actionAll()
     {
@@ -30,11 +31,43 @@ class ArticleController extends Controller
 
     public function actionOne($id)
     {
-        return 'article: ' . $id . '!';
+        // статический метод find возвращает запрос, QueryBuilder() - объект который позволяет построить запрос к БД
+        // а методы one или all позволяют выполнить запрос и получить либо массив объектов либо один объект указанного нами класса
+        $article = Article::find()->where(['id' => $id])->all();
+        var_dump($article);
     }
 
     public function actionShowOne()
     {
-        return 'hello, saf';
+        //Выбор всех записей из таблицы Article
+        //$articles = Article::find()->all(); //var_dump($articles);
+
+        // Сортировка записей в обратном порядке
+        //$articles = Article::find()->orderBy(['id' => SORT_DESC])->all();
+
+        // Данные в виде массива. в БД выполняется только один запрос.
+        //$articles = Article::find()->asArray()->all();
+
+        // Фильтровать данные по условию
+        //$articles = Article::find()->where(['title' => 'this is tests', 'id' => 3])->all();
+        //$articles = Article::find()->where('id=4')->all();
+        //$articles = Article::find()->where(['like', 'title', 't'])->all();
+        //$articles = Article::find()->where(['>', 'id', 3])->all();
+        //$articles = Article::find()->where(['title' => 'this is tests'])->limit(2)->all();
+        //$articles = Article::find()->asArray()->where(['title' => 'this is tests'])->limit(1)->one();
+        //$articles = Article::find()->asArray()->where(['title' => 'this is tests'])->count();
+        //$articles = Article::find()->asArray()->count();
+        //$articles = Article::findOne(['id' => 1] );
+        //$articles = Article::findAll(['title' => 'this is tests']);
+
+        //$sql = 'SELECT * FROM articles WHERE title LIKE \'%th%\'';
+        //$articles = Article::findBySql($sql)->all();
+
+        // Чтобы не было SQL иньекции необходимо пользоваться подготовленными запросами
+        //$sql = 'SELECT * FROM articles WHERE title LIKE :search';
+        //$articles = Article::findBySql($sql, [':search' => '%th%'])->all();
+        $articles = Article::find()->where(['id' => 1])->all();
+        var_dump($articles);
+        //return 'hello, saf';
     }
 }
